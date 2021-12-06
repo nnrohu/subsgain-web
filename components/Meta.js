e9e3f1a27182c40423e4c3ad450f77c4fb7f3b51
Rohit Kumar <nnrohu@gmail.com>
2021-12-06 20:18:23 +0530

    seo added

--- components/Meta.js
+++ components/Meta.js
@@ -1,14 +1,17 @@
 import Head from "next/head"
 function Meta({title, keywords, description}) {
     return (
-        <div>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name="keywords" content={keywords} />
-            <meta name="description" content={description} />
-            <meta charSet='utf-8' />
-            <link rel="icon" href='/favicon.ico' />
-            <title>{title}</title>
-        </div>
+      <div>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="keywords" content={keywords} />
+        <meta name="description" content={description} />
+        <meta charSet="utf-8" />
+        <meta property="og:title" content={`${title} - Subs Gain`} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>{title}</title>
+      </div>
     )
 }
 
